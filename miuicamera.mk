8795947b540a455565677d68c8e07748f0e0a1c5
Karan Parashar <karan@pixelos.net>
2023-07-17 00:38:17 +0200

    lisa-miuicamera: Set ro.miui.notch property
    Change-Id: I8608ea3140ef630ac8279ce15959a46d4576dfac

--- miuicamera.mk
+++ miuicamera.mk
@@ -14,6 +14,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.vendor.camera.privapp.list=org.codeaurora.snapcam,com.android.camera \
     ro.com.google.lens.oem_camera_package=com.android.camera \
+    ro.miui.notch=1 \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera
 
 # Sepolicy
